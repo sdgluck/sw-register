f31dc4d91e026c0557935493a03110fabbd0f93f
Sam Gluck <sdgluck@gmail.com>
2016-06-10 10:19:13 +0100

    Add check for navigator

--- index.js
+++ index.js
@@ -11,38 +11,40 @@ if (typeof define === 'function' && define.amd) {
 }
 
 function register (options) {
-  if (!('serviceWorker' in navigator)) {
+  if (!navigator) {
+    return Promise.reject(new Error('Not a browser context'))
+  } else if (!('serviceWorker' in navigator)) {
     return Promise.reject(new Error('Service Workers unsupported'))
   }
-  
+
   var serviceWorker = navigator.serviceWorker.controller
 
   return Promise.resolve()
-      .then(function () {
-        // Get existing service worker or get registration promise
-        if (serviceWorker) return serviceWorker
-        else if (!options) return navigator.serviceWorker.ready
-      })
-      .then(function (registration) {
-        if (registration) {
-          // Take this service worker that the registration returned
-          serviceWorker = registration
-        } else if (!registration && options) {
-          // No registration but we have options to register one
-          return navigator.serviceWorker
-              .register(options.url, options)
-              .then(function (registration) {
-                options.forceUpdate && registration.update()
-              })
-        } else if (!registration && !options) {
-          // No existing worker,
-          // no registration that returned one,
-          // no options to register one
-          throw new Error(
-            'no active service worker or configuration passed to install one'
-          )
-        }
-      })
+    .then(function () {
+      // Get existing service worker or get registration promise
+      if (serviceWorker) return serviceWorker
+      else if (!options) return navigator.serviceWorker.ready
+    })
+    .then(function (registration) {
+      if (registration) {
+        // Take this service worker that the registration returned
+        serviceWorker = registration
+      } else if (!registration && options) {
+        // No registration but we have options to register one
+        return navigator.serviceWorker
+            .register(options.url, options)
+            .then(function (registration) {
+              options.forceUpdate && registration.update()
+            })
+      } else if (!registration && !options) {
+        // No existing worker,
+        // no registration that returned one,
+        // no options to register one
+        throw new Error(
+          'no active service worker or configuration passed to install one'
+        )
+      }
+    })
     .then(function () {
       return serviceWorker
     })
--- test.js
+++ test.js
@@ -1,1 +1,37 @@
-// TODO write tests
+var test = require('tape')
+var sinon = require('sinon')
+
+var navigator = {
+  serviceWorker: {
+    register: function () {}
+  }
+}
+
+test('register typeof function', function (t) {
+  t.equal(typeof register, 'function')
+  t.end()
+})
+
+test('no args gets existing controller', function (t) {
+  t.end()
+})
+
+test('with options registers new worker', function (t) {
+  var sw = sinon.spy(navigator.serviceWorker, 'register')
+
+  var registration = {
+    url: 'url',
+    scope: 'scope',
+    forceUpdate: false
+  }
+
+  register().then(function () {
+    t.equal(true, navigator.serviceWorker.register.calledWith({
+      url: 'url',
+      scope: 'scope',
+      forceUpdate: false
+    }))
+    navigator.serviceWorker.restore()
+    t.end()
+  })
+})
