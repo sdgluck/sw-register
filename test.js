2fb3b41f88bffe6d149c7a15bfa4bac50de464b3
Sam Gluck <sdgluck@gmail.com>
2016-06-10 10:46:28 +0100

    Add tests

--- test.js
+++ test.js
@@ -25,10 +25,16 @@ test('no args gets existing controller', function (t) {
   })
 })
 
+test('no args no existing controller throws', function (t) {
+  navigator.serviceWorker.controller = null
+  register().catch(function (err) {
+    t.equal(err.message, 'no active service worker or configuration passed to install one')
+    t.end()
+  })
+})
+
 test('with options registers new worker', function (t) {
   var spy = sinon.spy(navigator.serviceWorker, 'register')
-
-  navigator.serviceWorker.controller = null
 
   var registration = {
     url: 'url',
